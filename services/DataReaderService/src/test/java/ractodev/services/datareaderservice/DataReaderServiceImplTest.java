93905d7fb4d16f383eecc648712055bdc23fc071
ractodev <erik.j.winbladh@gmail.com>
2024-03-06 17:12:36 +0100

    Update tests to find test resources

--- services/DataReaderService/src/test/java/ractodev/services/datareaderservice/DataReaderServiceImplTest.java
+++ services/DataReaderService/src/test/java/ractodev/services/datareaderservice/DataReaderServiceImplTest.java
@@ -5,20 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataReaderServiceImplTest {
-    private DataReaderServiceImpl dataReaderService;
-
-    /**
-     * Sets up the test environment by creating a new DataReaderServiceImpl object.
-     */
-    @BeforeEach
-    public void setUp() {
-        this.dataReaderService = new DataReaderServiceImpl();
-    }
 
     /**
      * Tests that getRawVideos returns the expected list of raw videos.
@@ -26,9 +18,11 @@ public class DataReaderServiceImplTest {
     @Test
     @DisplayName("Test getRawVideos valid input")
     public void testGetRawVideosSuccess() {
-        URL resource = getClass().getClassLoader().getResource("valid_history.json");
-        assert resource != null;
-        dataReaderService.setFilePath(resource.getPath());
+        String path = "src/test/resources/valid_history.json";
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+        DataReaderServiceImpl dataReaderService = new DataReaderServiceImpl();
+        dataReaderService.setFilePath(absolutePath);
 
         List<RawVideo> expectedRawVideos = List.of(new RawVideo("1"), new RawVideo("2"), new RawVideo("3"));
         List<RawVideo> actualRawVideos = dataReaderService.getRawVideos();
@@ -41,9 +35,11 @@ public class DataReaderServiceImplTest {
     @Test
     @DisplayName("Test getRawVideos empty input")
     public void testGetRawVideosEmpty() {
-        URL resource = getClass().getClassLoader().getResource("invalid_history.json");
-        assert resource != null;
-        dataReaderService.setFilePath(resource.getPath());
+        String path = "src/test/resources/invalid_history.json";
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+        DataReaderServiceImpl dataReaderService = new DataReaderServiceImpl();
+        dataReaderService.setFilePath(absolutePath);
 
         List<RawVideo> expectedRawVideos = new ArrayList<>();
         List<RawVideo> actualRawVideos = dataReaderService.getRawVideos();
@@ -56,6 +52,7 @@ public class DataReaderServiceImplTest {
     @Test
     @DisplayName("Test getRawVideos invalid input")
     public void testGetRawVideosInvalid() {
+        DataReaderServiceImpl dataReaderService = new DataReaderServiceImpl();
         dataReaderService.setFilePath("does_not_exist.json");
 
         List<RawVideo> actualRawVideos = dataReaderService.getRawVideos();
